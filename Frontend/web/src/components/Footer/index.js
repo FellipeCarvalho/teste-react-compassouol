f2b81c83a2975f184857b3b4ad355fb5293b95f5
Fellipe Carvalho <fellipealbert3@gmail.com>
2021-03-15 23:11:18 -0300

    Update name Footer Compasso UOL

--- Frontend/web/src/components/Footer/index.js
+++ Frontend/web/src/components/Footer/index.js
@@ -5,7 +5,7 @@ import * as S from './style'
   function Footer() {
     return (
             <S.Container>
-              <span>New ways to bring news for you - UOL Compasso</span>
+              <span>New ways to bring news for you - Compasso UOL</span>
             </S.Container>
           )
   }
